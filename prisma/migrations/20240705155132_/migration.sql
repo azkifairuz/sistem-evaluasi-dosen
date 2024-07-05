d883c5c35c07d9c7547803242780408cda4af948
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-05 22:52:09 +0700

    feat: dosen login

---
+++ prisma/migrations/20240705155132_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Account` MODIFY `token` VARCHAR(255) NULL;
--- src/auth-dosen/auth-dosen.controller.ts
+++ src/auth-dosen/auth-dosen.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthDosenService } from './auth-dosen.service';
+import { DosenLoginRequest, DosenLoginResponse } from 'src/model/Dosen.model';
+import { BaseResponse } from 'src/model/BaseResponse.model';
 
 @Controller('auth-dosen')
-export class AuthDosenController {}
+export class AuthDosenController {
+  constructor(private dosenAuthService: AuthDosenService) {}
+
+  @Post('/login')
+  async loginDosen(
+    @Body() request: DosenLoginRequest,
+  ): Promise<BaseResponse<DosenLoginResponse>> {
+    return await this.dosenAuthService.loginDosen(request);
+  }
+}
