41420a36d6412c613c5e96b2cde62f20a20640a3
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-07 01:06:20 +0700

    feat: get profile dosen

--- src/dosen-profile/dosen-profile.controller.ts
+++ src/dosen-profile/dosen-profile.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { DosenProfileService } from './dosen-profile.service';
+import { Authentication } from 'src/common/auth.decorator';
+import { Account } from '@prisma/client';
+import { BaseResponse } from 'src/model/BaseResponse.model';
+import { DosenProfile } from 'src/model/Dosen.model';
 
-@Controller('dosen-profile')
-export class DosenProfileController {}
+@Controller('dosen/profile')
+export class DosenProfileController {
+  constructor(private dosenProfileService: DosenProfileService) {}
+
+  @Get()
+  async getDosenProfile(
+    @Authentication() account: Account,
+  ): Promise<BaseResponse<DosenProfile>> {
+    return await this.dosenProfileService.getProfile(account);
+  }
+}
